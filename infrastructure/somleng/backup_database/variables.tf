8bd0b1b95357bd0362ee7d2fb016118175932405
David Wilkie <dwilkie@gmail.com>
2025-12-12 16:05:24 +0700

    Add staging database (#112)

--- infrastructure/somleng/backup_database/variables.tf
+++ infrastructure/somleng/backup_database/variables.tf
@@ -2,18 +2,27 @@ variable "aws_default_region" {
   default = "ap-southeast-1"
 }
 
+variable "instance_type" {
+  default = "t4g.small"
+
+  validation {
+    condition     = can(regex("^(t3|t4g)\\.", var.instance_type))
+    error_message = "instance_type must start with \"t3.\" or \"t4g.\""
+  }
+}
+
 variable "backup_db" {
   description = "y or n"
 }
 
 variable "db_name" {
-  description = "somleng or scfm"
+  description = "somleng, somleng_staging or scfm"
 }
 
 variable "cluster_identifier" {
-  description = "somlengv2 or open-ews"
+  description = "somlengv2, somleng-staging, or open-ews"
 }
 
 variable "db_master_password_parameter_identifier" {
-  description = "somleng or open-ews"
+  description = "somleng, somleng-staging, or open-ews"
 }
