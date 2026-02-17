519157d686ba97b461d96ba034c7e7dec012a6e6
Adham Salama <adham.s.salama@gmail.com>
2026-02-17 09:44:48 +0400

    Added SIP Registeration Trunk

--- somleng_patches/db/migrate/20260217120000_add_outbound_proxy_and_auth_user_to_sip_trunks.rb
+++ somleng_patches/db/migrate/20260217120000_add_outbound_proxy_and_auth_user_to_sip_trunks.rb
@@ -1,4 +1,4 @@
-class AddOutboundProxyAndAuthUserToSipTrunks < ActiveRecord::Migration[8.0]
+class AddOutboundProxyAndAuthUserToSIPTrunks < ActiveRecord::Migration[8.0]
   def change
     add_column :sip_trunks, :outbound_proxy, :string
     add_column :sip_trunks, :auth_user, :string
